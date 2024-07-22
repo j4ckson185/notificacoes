a334513dd06c4290e61a519a8f1e570b70aaf9f1
j4ckson185 <jackson_division@hotmail.com>
2024-07-22 11:56:15 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,45 +1,27 @@
-importScripts('https://www.gstatic.com/firebasejs/9.0.2/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.2/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging.js');
 
-const firebaseConfig = {
-    apiKey: "AIzaSyB-pF2lRStLTN9Xw9aYQj962qdNFyUXI2E",
-    authDomain: "cabana-8d55e.firebaseapp.com",
-    databaseURL: "https://cabana-8d55e-default-rtdb.firebaseio.com",
-    projectId: "cabana-8d55e",
-    storageBucket: "cabana-8d55e.appspot.com",
-    messagingSenderId: "706144237954",
-    appId: "1:706144237954:web:345c10370972486afc779b",
-    measurementId: "G-96Y337GYT8"
-};
-
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp({
+  apiKey: "AIzaSyB-pF2lRStLTN9Xw9aYQj962qdNFyUXI2E",
+  authDomain: "cabana-8d55e.firebaseapp.com",
+  databaseURL: "https://cabana-8d55e-default-rtdb.firebaseio.com",
+  projectId: "cabana-8d55e",
+  storageBucket: "cabana-8d55e.appspot.com",
+  messagingSenderId: "706144237954",
+  appId: "1:706144237954:web:345c10370972486afc779b",
+  measurementId: "G-96Y337GYT8"
+});
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: 'https://i.ibb.co/jZ6rbSp/logo-cabana.png'
-    };
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/firebase-logo.png'
+  };
 
-    self.registration.showNotification(notificationTitle, notificationOptions);
-});
-
-self.addEventListener('install', event => {
-    console.log('Service Worker instalado');
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-    console.log('Service Worker ativado');
-});
-
-self.addEventListener('notificationclick', event => {
-    event.notification.close();
-});
-
-self.addEventListener('notificationclose', event => {
-    console.log('Notificação fechada');
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
 });

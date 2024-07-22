00fbc6efc50c3864019fc8d3fbdc0ca43a80781d
j4ckson185 <jackson_division@hotmail.com>
2024-07-22 11:59:25 -0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,30 +1,47 @@
-// admin.js
-document.getElementById('sendMessageForm').addEventListener('submit', function(e) {
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+import { getMessaging } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging.js";
+
+const db = getFirestore();
+const messaging = getMessaging();
+
+document.getElementById('sendMessageForm').addEventListener('submit', async function(e) {
     e.preventDefault();
 
     const motoboy = document.getElementById('motoboySelect').value;
     const message = document.getElementById('messageInput').value;
 
     if (message) {
-        // Envie a notificação para o servidor (implemente a lógica do servidor)
-        // Exemplo:
-        fetch('/send-notification', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                motoboy: motoboy,
-                message: message
-            })
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Mensagem enviada com sucesso:', data);
-            document.getElementById('messageInput').value = '';
-        })
-        .catch((error) => {
-            console.error('Erro ao enviar a mensagem:', error);
+        const tokensRef = collection(db, "tokens");
+        const querySnapshot = await getDocs(tokensRef);
+        let token = '';
+
+        querySnapshot.forEach((doc) => {
+            if (doc.data().uid === motoboy) {
+                token = doc.data().token;
+            }
         });
+
+        if (token) {
+            fetch('/send-notification', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    token: token,
+                    message: message
+                })
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Mensagem enviada com sucesso:', data);
+                document.getElementById('messageInput').value = '';
+            })
+            .catch((error) => {
+                console.error('Erro ao enviar a mensagem:', error);
+            });
+        } else {
+            console.error('Token não encontrado para o motoboy:', motoboy);
+        }
     }
 });

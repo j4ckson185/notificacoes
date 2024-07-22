14a1e888c82c10f2cb26ff1784c7a084a36bd6c6
j4ckson185 <jackson_division@hotmail.com>
2024-07-22 11:41:58 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,14 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-app-compat.js';
-import { getDatabase, ref, push, onChildAdded, remove } from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-database-compat.js';
-import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-auth-compat.js';
-import { getMessaging, getToken } from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-messaging-compat.js';
-
+// firebase-config.js
 const firebaseConfig = {
     apiKey: "AIzaSyB-pF2lRStLTN9Xw9aYQj962qdNFyUXI2E",
     authDomain: "cabana-8d55e.firebaseapp.com",
+    databaseURL: "https://cabana-8d55e-default-rtdb.firebaseio.com",
     projectId: "cabana-8d55e",
     storageBucket: "cabana-8d55e.appspot.com",
     messagingSenderId: "706144237954",
     appId: "1:706144237954:web:345c10370972486afc779b",
-    databaseURL: "https://cabana-8d55e-default-rtdb.firebaseio.com"
+    measurementId: "G-96Y337GYT8"
 };
 
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-const auth = getAuth(app);
-const messaging = getMessaging(app);
-
-export { database, ref, push, onChildAdded, remove, auth, signInWithEmailAndPassword, onAuthStateChanged, signOut, messaging, getToken };
+// Inicialize o Firebase
+firebase.initializeApp(firebaseConfig);

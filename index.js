85114c5ef1d9359a910820f19986ca4c7dd7adc6
j4ckson185 <jackson_division@hotmail.com>
2024-07-22 03:52:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,61 @@
-import { auth, signInWithEmailAndPassword, database, ref, set, messaging, getToken } from './firebase-config.js';
+import { auth, signInWithEmailAndPassword, onAuthStateChanged, database, ref, set, messaging, getToken } from './firebase-config.js';
 
 const loginForm = document.getElementById('loginForm');
 
 // Lógica de Login
-loginForm.addEventListener('submit', async (e) => {
+loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const email = document.getElementById('loginEmail').value;
     const password = document.getElementById('loginPassword').value;
-    try {
-        const userCredential = await signInWithEmailAndPassword(auth, email, password);
-        loginForm.reset();
-        const user = userCredential.user;
-        saveTokenAndRedirect(user);
-    } catch (error) {
-        alert('Erro ao entrar: ' + error.message);
-    }
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            loginForm.reset();
+            const user = userCredential.user;
+            saveTokenAndRedirect(user);
+        })
+        .catch((error) => {
+            alert('Erro ao entrar: ' + error.message);
+        });
 });
 
-async function saveTokenAndRedirect(user) {
-    try {
-        const currentToken = await getToken(messaging, { vapidKey: 'BG1rGdXly1ZZLYgvdoo8M-yOxMULPxbt5f5WpbISG4XWChaV7AOyG4SjTsnSvAQlRI6Nwa5XurzTEvE8brQh01w' });
-        if (currentToken) {
-            console.log('Token FCM:', currentToken);
-            await set(ref(database, 'tokens/' + user.uid), {
-                token: currentToken
-            });
-            // Redirecionar com base no email
-            const emailToPageMap = {
-                'joaofmarcelino1@gmail.com': 'joaofelipe.html',
-                'giovanni.silva18@gmail.com': 'geovane.html',
-                'moises110723@gmail.com': 'moises.html',
-                'felipeaugusto02001@gmail.com': 'felipeaugusto.html',
-                'gurgel6901@icloud.com': 'pedro.html',
-                'jackson_division@hotmail.com': 'jackson.html'
-            };
-            const redirectPage = emailToPageMap[user.email];
-            if (redirectPage) {
-                window.location.href = redirectPage;
+function saveTokenAndRedirect(user) {
+    getToken(messaging, { vapidKey: 'BG1rGdXly1ZZLYgvdoo8M-yOxMULPxbt5f5WpbISG4XWChaV7AOyG4SjTsnSvAQlRI6Nwa5XurzTEvE8brQh01w' })
+        .then((currentToken) => {
+            if (currentToken) {
+                console.log('Token FCM:', currentToken);
+                set(ref(database, 'tokens/' + user.uid), {
+                    token: currentToken
+                });
+                // Redirecionar com base no email
+                const emailToPageMap = {
+                    'joaofmarcelino1@gmail.com': 'joaofelipe.html',
+                    'giovanni.silva18@gmail.com': 'geovane.html',
+                    'moises110723@gmail.com': 'moises.html',
+                    'felipeaugusto02001@gmail.com': 'felipeaugusto.html',
+                    'gurgel6901@icloud.com': 'pedro.html',
+                    'jackson_division@hotmail.com': 'jackson.html'
+                };
+                const redirectPage = emailToPageMap[user.email];
+                if (redirectPage) {
+                    window.location.href = redirectPage;
+                } else {
+                    alert('Página de destino não encontrada para o email: ' + user.email);
+                }
             } else {
-                alert('Página de destino não encontrada para o email: ' + user.email);
+                console.log('No registration token available. Request permission to generate one.');
             }
-        } else {
-            console.log('No registration token available. Request permission to generate one.');
-        }
-    } catch (err) {
-        console.log('An error occurred while retrieving token. ', err);
-    }
+        })
+        .catch((err) => {
+            console.log('An error occurred while retrieving token. ', err);
+        });
 }
+
+// Verifica o estado de autenticação do usuário
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        saveTokenAndRedirect(user);
+    }
+});
 
 // Registrar o Service Worker
 if ('serviceWorker' in navigator) {

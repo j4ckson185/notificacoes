9cb00ca70c499bd80f61f49df3ead68da4620536
j4ckson185 <jackson_division@hotmail.com>
2024-07-22 03:22:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -63,7 +63,6 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/firebase-messaging-sw.js')
         .then((registration) => {
             console.log('Service Worker registrado com sucesso, escopo é:', registration.scope);
-            messaging.useServiceWorker(registration);
         }).catch((err) => {
             console.error('Falha ao registrar o Service Worker:', err);
         });

8ebe93a93de1db0051b4b4e09f28795fbcc8ae42
j4ckson185 <jackson_division@hotmail.com>
2024-07-22 12:14:11 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,5 @@
-export const firebaseConfig = {
+// Não use "export" neste arquivo. Em vez disso, defina a configuração diretamente.
+const firebaseConfig = {
   apiKey: "AIzaSyB-pF2lRStLTN9Xw9aYQj962qdNFyUXI2E",
   authDomain: "cabana-8d55e.firebaseapp.com",
   databaseURL: "https://cabana-8d55e-default-rtdb.firebaseio.com",
@@ -8,3 +9,9 @@ export const firebaseConfig = {
   appId: "1:706144237954:web:345c10370972486afc779b",
   measurementId: "G-96Y337GYT8"
 };
+
+// Inicialize o Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const messaging = firebase.messaging();
+const db = firebase.firestore();

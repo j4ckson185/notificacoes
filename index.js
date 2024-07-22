216b6277e7ce052159bbb7c0fb4c2cb8f68d34e8
j4ckson185 <jackson_division@hotmail.com>
2024-07-22 11:49:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 // index.js
-document.getElementById('loginForm').addEventListener('submit', function(e) {
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+// Substitua pelas configurações do seu projeto Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyB-pF2lRStLTN9Xw9aYQj962qdNFyUXI2E",
+    authDomain: "cabana-8d55e.firebaseapp.com",
+    databaseURL: "https://cabana-8d55e-default-rtdb.firebaseio.com",
+    projectId: "cabana-8d55e",
+    storageBucket: "cabana-8d55e.appspot.com",
+    messagingSenderId: "706144237954",
+    appId: "1:706144237954:web:345c10370972486afc779b",
+    measurementId: "G-96Y337GYT8"
+};
+
+// Inicialize o Firebase
+const app = initializeApp(firebaseConfig);
+
+// Obtenha uma referência para o serviço de autenticação
+const auth = getAuth(app);
+
+// Manipule o formulário de login
+document.getElementById('loginForm').addEventListener('submit', async (e) => {
     e.preventDefault();
 
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    // Autenticação com Firebase (use o SDK do Firebase)
-    // Exemplo:
-    firebase.auth().signInWithEmailAndPassword(email, password)
-        .then((userCredential) => {
-            // Sucesso! Redirecione para a página de painel administrativo
-            window.location.href = 'admin.html';
-        })
-        .catch((error) => {
-            console.error('Erro ao fazer login:', error);
-        });
+    try {
+        // Crie um novo usuário com email e senha
+        await createUserWithEmailAndPassword(auth, email, password);
+        console.log('Usuário registrado com sucesso!');
+        // Redirecione para a próxima página (por exemplo, o painel administrativo)
+        // window.location.href = 'admin.html';
+    } catch (error) {
+        console.error('Erro ao registrar usuário:', error.message);
+    }
 });

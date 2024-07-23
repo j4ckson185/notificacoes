911c3211b46830ce8489bae65ffcf25ccc7d8592
j4ckson185 <jackson_division@hotmail.com>
2024-07-23 03:58:26 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,8 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
-import { getMessaging, getToken, onMessage } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging.js';
+import { getMessaging, getToken } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging.js';
 import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js';
-import { getFirestore, collection, doc, setDoc, getDocs } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js';
+import { getFirestore, collection, doc, getDocs, onSnapshot, deleteDoc } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js';
+import { getDatabase, ref, push, onChildAdded, remove, set } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js';
 
 const firebaseConfig = {
     apiKey: "AIzaSyB-pF2lRStLTN9Xw9aYQj962qdNFyUXI2E",
@@ -16,31 +17,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
 const auth = getAuth(app);
+const database = getDatabase(app);
 const firestore = getFirestore(app);
 
-async function saveTokenToDatabase(token, userId) {
-    try {
-        await setDoc(doc(firestore, 'tokens', userId), { token, userId });
-    } catch (error) {
-        console.error('Erro ao salvar o token no Firestore:', error);
-    }
-}
-
-async function requestPermission(userId) {
-    try {
-        const permission = await Notification.requestPermission();
-        if (permission === 'granted') {
-            const token = await getToken(messaging, { vapidKey: 'BG1rGdXly1ZZLYgvdoo8M-yOxMULPxbt5f5WpbISG4XWChaV7AOyG4SjTsnSvAQlRI6Nwa5XurzTEvE8brQh01w' });
-            if (token) {
-                console.log('Token FCM:', token);
-                await saveTokenToDatabase(token, userId);
-            }
-        } else {
-            console.log('Permissão para notificações negada');
-        }
-    } catch (error) {
-        console.error('Erro ao obter o token FCM:', error);
-    }
-}
-
-export { app, messaging, getMessaging, getToken, onMessage, auth, signInWithEmailAndPassword, onAuthStateChanged, signOut, firestore, collection, doc, setDoc, getDocs, requestPermission };
+export { app, messaging, getToken, auth, signInWithEmailAndPassword, onAuthStateChanged, signOut, database, ref, push, onChildAdded, remove, set, firestore, collection, doc, getDocs, onSnapshot, deleteDoc };

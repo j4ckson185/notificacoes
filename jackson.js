455cfd0702ec89040c9f22c2895b40eca1bc533b
j4ckson185 <jackson_division@hotmail.com>
2024-07-23 20:04:29 -0300

    Update jackson.js

--- jackson.js
+++ jackson.js
@@ -1,14 +1,20 @@
-import { getDatabase, ref, onChildAdded } from './firebase-config.js';
+import { getDatabase, ref, onValue } from './firebase-config.js';
 
 const messagesContainer = document.getElementById('messages-container');
 
 // Referência para o nó "messages" no Realtime Database
 const messagesRef = ref(getDatabase(), 'messages');
 
-// Ouvir por novas mensagens adicionadas
-onChildAdded(messagesRef, (snapshot) => {
-    const messageData = snapshot.val();
-    const messageElement = document.createElement('div');
-    messageElement.textContent = messageData.text;
-    messagesContainer.appendChild(messageElement);
+// Ouvir por mudanças no nó "messages"
+onValue(messagesRef, (snapshot) => {
+    // Limpar o conteúdo do container de mensagens
+    messagesContainer.innerHTML = '';
+
+    // Iterar sobre os filhos do nó "messages"
+    snapshot.forEach((childSnapshot) => {
+        const messageData = childSnapshot.val();
+        const messageElement = document.createElement('div');
+        messageElement.textContent = messageData.text;
+        messagesContainer.appendChild(messageElement);
+    });
 });

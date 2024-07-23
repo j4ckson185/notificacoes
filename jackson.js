df393c842d5dc00fa2794ca27098d4424258c6b0
j4ckson185 <jackson_division@hotmail.com>
2024-07-23 03:10:39 -0300

    Update jackson.js

--- jackson.js
+++ jackson.js
@@ -1,4 +1,4 @@
-import { app, messaging, auth, signInWithEmailAndPassword, onAuthStateChanged, signOut, database, ref, onChildAdded, remove, onMessage } from './firebase-config.js';
+import { messaging, auth, signInWithEmailAndPassword, onAuthStateChanged, signOut, database, ref, onChildAdded, remove, onMessage } from './firebase-config.js';
 
 const loginForm = document.getElementById('loginForm');
 const logoutButton = document.getElementById('logoutButton');

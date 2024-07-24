899adaa111f6e92e2ca96c15c31c282fa301d2db
j4ckson185 <jackson_division@hotmail.com>
2024-07-24 01:25:34 -0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,36 +1,22 @@
-import { firestore, collection, getDocs, getDatabase, ref, set } from './firebase-config.js';
+import { getDatabase, ref, set } from './firebase-config.js';
 
 document.getElementById('sendMessageForm').addEventListener('submit', async function(e) {
     e.preventDefault();
 
-    const motoboy = document.getElementById('motoboySelect').value;
+    const user = document.getElementById('userSelect').value;
     const message = document.getElementById('messageInput').value;
 
     if (message) {
-        const tokensRef = collection(firestore, "tokens");
-        const querySnapshot = await getDocs(tokensRef);
-        let token = '';
-
-        querySnapshot.forEach((doc) => {
-            if (doc.data().uid === motoboy) {
-                token = doc.data().token;
-            }
+        // Send message to Realtime Database
+        set(ref(getDatabase(), `messages/${user}`), {
+            text: message
+        })
+        .then(() => {
+            console.log('Mensagem enviada com sucesso para o Realtime Database!');
+            document.getElementById('messageInput').value = '';
+        })
+        .catch((error) => {
+            console.error('Erro ao enviar a mensagem para o Realtime Database:', error);
         });
-
-        if (token) {
-            // Send message to Realtime Database
-            set(ref(getDatabase(), `messages/${motoboy}`), {
-                text: message
-            })
-            .then(() => {
-                console.log('Mensagem enviada com sucesso para o Realtime Database!');
-                document.getElementById('messageInput').value = '';
-            })
-            .catch((error) => {
-                console.error('Erro ao enviar a mensagem para o Realtime Database:', error);
-            });
-        } else {
-            console.error('Token não encontrado para o motoboy:', motoboy);
-        }
     }
 });

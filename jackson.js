16395ed889dbf43dcc7667a5cd03156d951dabe2
j4ckson185 <jackson_division@hotmail.com>
2024-07-24 01:02:08 -0300

    Update jackson.js

--- jackson.js
+++ jackson.js
@@ -1,43 +1,20 @@
-import { getDatabase, ref, onValue, getMessaging, getToken, onMessage, messaging } from './firebase-config.js';
+import { getDatabase, ref, onValue } from './firebase-config.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-    const messagesContainer = document.getElementById('messages-container');
+const messagesContainer = document.getElementById('messages-container');
 
-    // Referência para o nó "messages" no Realtime Database
-    const messagesRef = ref(getDatabase(), 'messages');
+// Referência para o nó "messages" no Realtime Database
+const messagesRef = ref(getDatabase(), 'messages');
 
-    // Ouvir por mudanças no nó "messages"
-    onValue(messagesRef, (snapshot) => {
-        if (messagesContainer) {
-            // Limpar o conteúdo do container de mensagens
-            messagesContainer.innerHTML = '';
+// Ouvir por mudanças no nó "messages"
+onValue(messagesRef, (snapshot) => {
+    // Limpar o conteúdo do container de mensagens
+    messagesContainer.innerHTML = '';
 
-            // Iterar sobre os filhos do nó "messages"
-            snapshot.forEach((childSnapshot) => {
-                const messageData = childSnapshot.val();
-                const messageElement = document.createElement('div');
-                messageElement.textContent = messageData.text;
-                messagesContainer.appendChild(messageElement);
-            });
-        }
-    });
-
-    // Ouvir por mensagens FCM
-    onMessage(messaging, (payload) => {
-        const messageData = payload.data;
+    // Iterar sobre os filhos do nó "messages"
+    snapshot.forEach((childSnapshot) => {
+        const messageData = childSnapshot.val();
         const messageElement = document.createElement('div');
         messageElement.textContent = messageData.text;
         messagesContainer.appendChild(messageElement);
-    });
-
-    // Obter o token FCM
-    getToken(messaging).then((currentToken) => {
-        if (currentToken) {
-            // Enviar o token para o servidor para salvar no Realtime Database
-            // ...
-        } else {
-            // Mostrar uma mensagem de erro
-            console.error('Erro ao obter o token FCM.');
-        }
     });
 });

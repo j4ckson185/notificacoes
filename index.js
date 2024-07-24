12e303ce1603338f344da8cf20835659913f8117
j4ckson185 <jackson_division@hotmail.com>
2024-07-24 00:20:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,20 @@ loginForm.addEventListener('submit', async (e) => {
 
     try {
         const userCredential = await signInWithEmailAndPassword(getAuth(), email, password);
+        const user = userCredential.user;
+        console.log('Usuário logado:', user);
+
+        // Redirecionar para a página específica com base no email
+        if (email === 'jackson_division@hotmail.com') {
+            window.location.href = 'jackson.html';
+        } else {
+            // Redirecionar para outra página ou mostrar uma mensagem de erro
+            console.error('Email inválido ou página não encontrada.');
+        }
+
+    } catch (error) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error('Erro ao fazer login:', errorCode, errorMessage);
+    }
+});

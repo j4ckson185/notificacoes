e8dbe6878a1acbeb559f73f0f2d86ab8fd8c78df
j4ckson185 <jackson_division@hotmail.com>
2024-07-24 03:24:25 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,18 +2,16 @@ importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging-compat.js');
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB-pF2lRStLTN9Xw9aYQj962qdNFyUXI2E", // Replace with your actual API key
-    authDomain: "cabana-8d55e.firebaseapp.com", // Replace with your actual auth domain
-    projectId: "cabana-8d55e", // Replace with your actual project ID
-    storageBucket: "cabana-8d55e.appspot.com", // Replace with your actual storage bucket
-    messagingSenderId: "706144237954", // Replace with your actual messaging sender ID
-    appId: "1:706144237954:web:345c10370972486afc779b", // Replace with your actual app ID
-    databaseURL: "https://cabana-8d55e-default-rtdb.firebaseio.com" // Replace with your actual database URL
-
+    apiKey: "AIzaSyB-pF2lRStLTN9Xw9aYQj962qdNFyUXI2E",
+    authDomain: "cabana-8d55e.firebaseapp.com",
+    projectId: "cabana-8d55e",
+    storageBucket: "cabana-8d55e.appspot.com",
+    messagingSenderId: "706144237954",
+    appId: "1:706144237954:web:345c10370972486afc779b",
+    databaseURL: "https://cabana-8d55e-default-rtdb.firebaseio.com"
 };
 
 firebase.initializeApp(firebaseConfig);
-
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
@@ -21,8 +19,8 @@ messaging.onBackgroundMessage((payload) => {
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
-        icon: 'https://i.ibb.co/jZ6rbSp/logo-cabana.png' // Replace with your icon URL
+        icon: 'https://i.ibb.co/jZ6rbSp/logo-cabana.png',
+        sound: '/assets/notification.mp3' // Certifique-se de que o caminho do som esteja correto
     };
-
     self.registration.showNotification(notificationTitle, notificationOptions);
 });

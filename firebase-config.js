f4f64f91b178485b8de135fdfbff04898f6c95f5
j4ckson185 <jackson_division@hotmail.com>
2024-07-25 18:46:06 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,4 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
-import { getDatabase, ref, set, push, onValue, onChildAdded } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js';
-import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js';
-import { getMessaging, getToken, onMessage } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging.js';
-
+// firebase-config.js
 const firebaseConfig = {
     apiKey: "AIzaSyB-pF2lRStLTN9Xw9aYQj962qdNFyUXI2E",
     authDomain: "cabana-8d55e.firebaseapp.com",
@@ -15,12 +11,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
 // Get Firebase services after initialization
-const database = getDatabase(app);
-const auth = getAuth(app);
-const messaging = getMessaging(app);
-
-export { app, database, auth, messaging, getDatabase, ref, set, push, onValue, onChildAdded, getToken, onMessage, signInWithEmailAndPassword, onAuthStateChanged, signOut };
-export { firebaseConfig };
+const database = firebase.database();
+const auth = firebase.auth();
+const messaging = firebase.messaging();

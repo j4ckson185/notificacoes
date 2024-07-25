acff8ba6de1468645fc1199712721dace706efde
j4ckson185 <jackson_division@hotmail.com>
2024-07-25 14:13:19 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -4,11 +4,12 @@ importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging-comp
 const firebaseConfig = {
     apiKey: "AIzaSyB-pF2lRStLTN9Xw9aYQj962qdNFyUXI2E",
     authDomain: "cabana-8d55e.firebaseapp.com",
+    databaseURL: "https://cabana-8d55e-default-rtdb.firebaseio.com",
     projectId: "cabana-8d55e",
     storageBucket: "cabana-8d55e.appspot.com",
     messagingSenderId: "706144237954",
     appId: "1:706144237954:web:345c10370972486afc779b",
-    databaseURL: "https://cabana-8d55e-default-rtdb.firebaseio.com"
+    measurementId: "G-96Y337GYT8"
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -19,25 +20,8 @@ messaging.onBackgroundMessage((payload) => {
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
-        icon: 'https://i.ibb.co/jZ6rbSp/logo-cabana.png',
-        data: {
-            sound: '/assets/notification.mp3',
-            click_action: 'https://cabananotificacoes.netlify.app/jackson.html' // URL para abrir ao clicar na notificação
-        }
+        icon: 'https://i.ibb.co/jZ6rbSp/logo-cabana.png'
     };
+
     self.registration.showNotification(notificationTitle, notificationOptions);
-});
-
-self.addEventListener('notificationclick', function(event) {
-    const sound = event.notification.data.sound;
-    event.notification.close();
-
-    // Open the URL when the user clicks on the notification
-    event.waitUntil(clients.openWindow(event.notification.data.click_action));
-
-    // Play sound
-    const audio = new Audio(sound);
-    audio.play().catch((error) => {
-        console.error('Error playing notification sound:', error);
-    });
 });

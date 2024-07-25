839d675c5621c1e12db93af08bc2649ec6066551
j4ckson185 <jackson_division@hotmail.com>
2024-07-25 19:54:26 -0300

    Update geolocation.js

--- geolocation.js
+++ geolocation.js
@@ -2,7 +2,7 @@ import { auth, database, ref, set } from './firebase-config.js';
 
 function updateLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position) => {
+        navigator.geolocation.watchPosition((position) => {
             const user = auth.currentUser;
             if (user) {
                 const userId = user.uid;
@@ -15,26 +15,12 @@ function updateLocation() {
         }, (error) => {
             console.error('Erro ao obter localização:', error);
         });
-
-        navigator.geolocation.watchPosition((position) => {
-            const user = auth.currentUser;
-            if (user) {
-                const userId = user.uid;
-                const userRef = ref(database, 'locations/' + userId);
-                set(userRef, {
-                    latitude: position.coords.latitude,
-                    longitude: position.coords.longitude
-                });
-            }
-        }, (error) => {
-            console.error('Erro ao atualizar localização:', error);
-        });
     } else {
         console.error('Geolocalização não é suportada pelo navegador.');
     }
 }
 
-// Chamar a função após o login do usuário
+// Call the function when the user logs in
 auth.onAuthStateChanged((user) => {
     if (user) {
         updateLocation();

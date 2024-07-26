f0b15c3cfe91044d5f9b6dc4335ab2148cfd6552
j4ckson185 <jackson_division@hotmail.com>
2024-07-26 20:36:07 -0300

    Update mapa.js

--- mapa.js
+++ mapa.js
@@ -1,7 +1,7 @@
-// mapa.js
-import { database } from './firebase-config.js';
-import { ref, onValue } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js';
+import { getDatabase, ref, onValue } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js';
+import { getAuth } from './firebase-config.js';
 
+const database = getDatabase();
 let map;
 
 function initMap() {
@@ -10,37 +10,7 @@ function initMap() {
         zoom: 15
     });
 
-    // Solicitar permissão de localização
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-                const pos = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude,
-                };
-
-                new google.maps.Marker({
-                    position: pos,
-                    map,
-                    icon: {
-                        url: "https://i.ibb.co/FHdgjcK/capacete.png", // Ícone do capacete
-                        scaledSize: new google.maps.Size(45, 45) // Tamanho do ícone
-                    },
-                    title: "Minha localização",
-                });
-
-                map.setCenter(pos);
-            },
-            () => {
-                handleLocationError(true, map.getCenter());
-            }
-        );
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, map.getCenter());
-    }
-
-    // Atualizar localização dos motoboys
+    // Atualizar localização dos usuários
     const locationsRef = ref(database, 'locations');
     onValue(locationsRef, (snapshot) => {
         snapshot.forEach((childSnapshot) => {
@@ -59,18 +29,6 @@ function initMap() {
             }
         });
     });
-}
-
-function handleLocationError(browserHasGeolocation, pos) {
-    const infoWindow = new google.maps.InfoWindow({
-        position: pos,
-    });
-    infoWindow.setContent(
-        browserHasGeolocation
-            ? "Error: The Geolocation service failed."
-            : "Error: Your browser doesn't support geolocation."
-    );
-    infoWindow.open(map);
 }
 
 // Expor initMap para ser chamado pela API do Google Maps

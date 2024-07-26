cb1b4ffddb75ad478e319baf59e669f73f22b6a0
j4ckson185 <jackson_division@hotmail.com>
2024-07-26 20:39:37 -0300

    Update mapa.js

--- mapa.js
+++ mapa.js
@@ -1,7 +1,21 @@
-import { getDatabase, ref, onValue } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js';
-import { getAuth } from './firebase-config.js';
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
 
-const database = getDatabase();
+const firebaseConfig = {
+    apiKey: "AIzaSyB-pF2lRStLTN9Xw9aYQj962qdNFyUXI2E",
+    authDomain: "cabana-8d55e.firebaseapp.com",
+    databaseURL: "https://cabana-8d55e-default-rtdb.firebaseio.com",
+    projectId: "cabana-8d55e",
+    storageBucket: "cabana-8d55e.appspot.com",
+    messagingSenderId: "706144237954",
+    appId: "1:706144237954:web:345c10370972486afc779b",
+    measurementId: "G-96Y337GYT8"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
 let map;
 
 function initMap() {

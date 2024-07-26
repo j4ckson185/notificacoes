ad4fd428722791acf0b8cf025b88e79d49c9c20c
j4ckson185 <jackson_division@hotmail.com>
2024-07-26 20:35:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,52 @@
-import { firebaseAuth } from './firebase-config.js';
-import { signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js';
+import { getAuth, signInWithEmailAndPassword } from './firebase-config.js';
+import { getDatabase, ref, set } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js';
 
-// Lidar com o envio do formulário de login
-document.getElementById('loginForm').addEventListener('submit', async function(e) {
+const auth = getAuth();
+const database = getDatabase();
+
+document.getElementById('loginForm').addEventListener('submit', (e) => {
     e.preventDefault();
+
     const email = document.getElementById('loginEmail').value;
     const password = document.getElementById('loginPassword').value;
 
-    try {
-        const userCredential = await signInWithEmailAndPassword(firebaseAuth, email, password);
-        const user = userCredential.user;
-        console.log('Usuário logado:', user);
-        // Redirecionar para a página principal após o login
-        window.location.href = 'mapa.html';
-    } catch (error) {
-        console.error('Erro ao fazer login:', error);
-        // Exibir mensagem de erro para o usuário
-        alert('Erro ao fazer login. Verifique suas credenciais e tente novamente.');
-    }
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            startTrackingLocation(user.uid);
+        })
+        .catch((error) => {
+            console.error('Error signing in:', error);
+        });
 });
+
+function startTrackingLocation(userId) {
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(
+            (position) => {
+                const pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                };
+
+                // Atualizar a localização do usuário no Firebase
+                set(ref(database, 'locations/' + userId), {
+                    lat: pos.lat,
+                    lng: pos.lng,
+                    name: userId // Ou qualquer identificador que você queira usar
+                });
+
+            },
+            (error) => {
+                console.error('Error getting location:', error);
+            },
+            {
+                enableHighAccuracy: true,
+                timeout: 10000,
+                maximumAge: 1000
+            }
+        );
+    } else {
+        console.error('Geolocation not supported.');
+    }
+}

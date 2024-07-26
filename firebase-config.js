c8336ba4584d02dcd3aa4115dd49243aea30d931
j4ckson185 <jackson_division@hotmail.com>
2024-07-26 20:24:06 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,10 @@
 // firebase-config.js
-
-// Importa os módulos Firebase necessários
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
 import { getDatabase } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js';
 import { getMessaging } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging.js';
 
-// Configuração do Firebase
+// Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyB-pF2lRStLTN9Xw9aYQj962qdNFyUXI2E",
     authDomain: "cabana-8d55e.firebaseapp.com",
@@ -18,10 +16,11 @@ const firebaseConfig = {
     measurementId: "G-96Y337GYT8"
 };
 
-// Inicializa o Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth(app);
+const messaging = getMessaging(app);
 
-// Exporta os serviços necessários
-export const database = getDatabase(app);
-export const auth = getAuth(app);
-export const messaging = getMessaging(app);
+// Export Firebase services
+export { database, auth, messaging };

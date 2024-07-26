74a91ba72b2ae9b0811e5cdc135d8f21a73a9897
j4ckson185 <jackson_division@hotmail.com>
2024-07-26 17:00:25 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,5 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
-import { getDatabase, ref, set, push, onValue, query, orderByChild, equalTo } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js';
-import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js';
-import { getMessaging, getToken, onMessage } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging.js';
-
+// firebase-config.js
+// Firebase Configuration
 const firebaseConfig = {
     apiKey: "AIzaSyB-pF2lRStLTN9Xw9aYQj962qdNFyUXI2E",
     authDomain: "cabana-8d55e.firebaseapp.com",
@@ -27,12 +24,9 @@ try {
     console.error("Firebase messaging is not available:", error);
 }
 
-// Expose the initialized services globally if needed
+// Expose the initialized services globally
 window.firebaseDatabase = database;
 window.firebaseAuth = auth;
 if (messaging) {
     window.firebaseMessaging = messaging;
 }
-
-export { app, database, auth, messaging, getDatabase, ref, set, push, onValue, query, orderByChild, equalTo, getToken, onMessage, signInWithEmailAndPassword, onAuthStateChanged, signOut };
-export { firebaseConfig };

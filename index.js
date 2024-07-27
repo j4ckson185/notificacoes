56eaba16ae9bcfd557eb8fb7be5ade429075bf2a
j4ckson185 <jackson_division@hotmail.com>
2024-07-26 21:38:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,41 @@
-// Substitua com suas configurações do Firebase
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
+// Configuração do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyB-pF2lRStLTN9Xw9aYQj962qdNFyUXI2E",
-  authDomain: "cabana-8d55e.firebaseapp.com",
-  databaseURL: "https://cabana-8d55e-default-rtdb.firebaseio.com",
-  projectId: "cabana-8d55e",
-  storageBucket: "cabana-8d55e.appspot.com",
-  messagingSenderId: "706144237954",
-  appId: "1:706144237954:web:345c10370972486afc779b",
-  measurementId: "G-96Y337GYT8"
+    apiKey: "AIzaSyB-pF2lRStLTN9Xw9aYQj962qdNFyUXI2E",
+    authDomain: "cabana-8d55e.firebaseapp.com",
+    databaseURL: "https://cabana-8d55e-default-rtdb.firebaseio.com",
+    projectId: "cabana-8d55e",
+    storageBucket: "cabana-8d55e.appspot.com",
+    messagingSenderId: "706144237954",
+    appId: "1:706144237954:web:345c10370972486afc779b",
+    measurementId: "G-96Y337GYT8"
 };
 
-// Inicialize o Firebase
-firebase.initializeApp(firebaseConfig);
+// Inicializa o Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-const auth = firebase.auth();
-
-document.querySelector('#loginForm').addEventListener('submit', async (e) => {
+const loginForm = document.getElementById('loginForm');
+loginForm.addEventListener('submit', async (e) => {
     e.preventDefault();
-    const email = document.querySelector('#loginEmail').value;
-    const password = document.querySelector('#loginPassword').value;
+    
+    const email = document.getElementById('loginEmail').value;
+    const password = document.getElementById('loginPassword').value;
 
     try {
-        await auth.signInWithEmailAndPassword(email, password);
-        console.log('Login bem-sucedido');
-        // Redirecionar ou realizar outra ação após login
-        window.location.href = 'mapa.html'; // Exemplo de redirecionamento
+        await signInWithEmailAndPassword(auth, email, password);
+        // Redireciona para mapa.html após login bem-sucedido
+        window.location.href = 'mapa.html';
     } catch (error) {
-        console.error('Erro ao fazer login', error);
+        console.error('Error signing in:', error);
     }
 });
 
-// Função para manter o usuário logado
-window.onload = function() {
-    auth.onAuthStateChanged(user => {
-        if (user) {
-            console.log('Usuário já autenticado');
-            // Redirecionar ou realizar outra ação após o login automático
-            window.location.href = 'mapa.html'; // Exemplo de redirecionamento
-        }
-    });
-};
+// Tenta manter o usuário logado ao carregar a página
+auth.onAuthStateChanged((user) => {
+    if (user) {
+        window.location.href = 'mapa.html';
+    }
+});
